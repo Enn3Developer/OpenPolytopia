db7f531d95cf33a2162e50bf78954392c95f81b0
Enn3Developer <arturpeshko39@gmail.com>
2025-02-26 13:00:41 +0100

    more ergonomic API + cleaned a bit the code

--- OpenPolytopia/src/Grid.cs
+++ OpenPolytopia/src/Grid.cs
@@ -14,7 +14,7 @@ public struct Tile {
   private const int RUIN_POSITION = 62;
   private const int TILEKIND_POSITION = 59;
   private const int TILE_MODIFIER_POSITION = 57;
-  private const int TILE_BUILDINGS_POSITION = 54;
+  private const int TILE_BUILDING_POSITION = 54;
 
   private const int ONE_BIT = 1;
   private const int TWO_BITS = 3;
@@ -54,54 +54,89 @@ public struct Tile {
   private ulong _inner;
 
   /// <summary>
+  /// Roads/bridge on top of this tile
+  /// </summary>
+  public bool Roads {
+    get => _inner.GetBits(ONE_BIT, ROAD_POSITION) == 1;
+    set => _inner.SetBits(value.ToULong(), ONE_BIT, ROAD_POSITION);
+  }
+
+  /// <summary>
+  /// Ruin on top of this tile
+  /// </summary>
+  public bool Ruin {
+    get => _inner.GetBits(ONE_BIT, RUIN_POSITION) == 1;
+    set => _inner.SetBits(value.ToULong(), ONE_BIT, RUIN_POSITION);
+  }
+
+  /// <summary>
+  /// The type of the tile
+  /// </summary>
+  public TileKind TileKind => (TileKind)_inner.GetBits(THREE_BITS, TILEKIND_POSITION);
+
+  /// <summary>
+  /// The tile modifier castable to the corresponding enum
+  /// </summary>
+  public int TileModifier {
+    get => (int)_inner.GetBits(TWO_BITS, TILE_MODIFIER_POSITION);
+    set => _inner.SetBits((ulong)value, TWO_BITS, TILE_MODIFIER_POSITION);
+  }
+
+  /// <summary>
+  /// The tile building castable to the corresponding enum
+  /// </summary>
+  public int TileBuilding {
+    get => (int)_inner.GetBits(THREE_BITS, TILE_BUILDING_POSITION);
+    set => _inner.SetBits((ulong)value, THREE_BITS, TILE_BUILDING_POSITION);
+  }
+
+  /// <summary>
   /// Creates a new tile from a <see cref="TileKind"/>
   /// </summary>
   /// <param name="kind">the type of tile to create</param>
-  /// <returns>a new tile</returns>
-  public static Tile FromTileKind(TileKind kind) => new() { _inner = (ulong)kind << TILEKIND_POSITION };
-
-  /// <summary>
-  /// Checks if the tile has a road/bridge
-  /// </summary>
-  /// <returns>whether there's a road or a bridge on this tile</returns>
-  public bool HasRoad() => _inner >> ROAD_POSITION == 1;
-
-  /// <summary>
-  /// Sets the road for this tile
-  /// </summary>
-  /// <param name="road">1: yes road; 0: no road</param>
-  public void SetRoad(bool road) => _inner |= road.ToULong() << ROAD_POSITION;
-
-  /// <summary>
-  /// Checks if the tile has a ruin
-  /// </summary>
-  /// <returns>whether there's a ruin or not</returns>
-  public bool HasRuin() => ((_inner >> RUIN_POSITION) & ONE_BIT) == 1;
-
-  /// <summary>
-  /// Sets the ruin for this tile
-  /// </summary>
-  /// <param name="ruin">1: yes ruin; 0: no ruin</param>
-  public void SetRuin(bool ruin) => _inner |= ruin.ToULong() << RUIN_POSITION;
-
-  /// <summary>
-  /// Gets the tile's type
-  /// </summary>
-  /// <returns>the tile's type</returns>
-  public TileKind GetTileKind() => (TileKind)((_inner >> TILEKIND_POSITION) & THREE_BITS);
-
-  /// <summary>
-  /// Returns the tile modifier
-  /// </summary>
-  /// <returns>the tile modifier as an <c>int</c></returns>
-  public int GetTileModifier() => (int)((_inner >> TILE_MODIFIER_POSITION) & TWO_BITS);
+  public Tile(TileKind kind) {
+    _inner = (ulong)kind << TILEKIND_POSITION;
+  }
 
   /// <summary>
   /// Returns the tile modifier
   /// </summary>
   /// <typeparam name="T">The tile modifier enum</typeparam>
   /// <returns>the tile modifier as <c>T</c></returns>
-  public T GetTileModifier<T>() => (T)(object)GetTileModifier();
+  public T GetTileModifier<T>() => (T)(object)TileModifier;
+
+  /// <summary>
+  /// Sets the tile modifier
+  /// </summary>
+  /// <param name="tileModifier">the tile modifier enum value</param>
+  public void SetTileModifier(object tileModifier) => TileModifier = (int)tileModifier;
+
+  /// <summary>
+  /// Sets the tile modifier
+  /// </summary>
+  /// <param name="tileModifier">the tile modifier enum value</param>
+  /// <typeparam name="T">The tile modifier enum</typeparam>
+  public void SetTileModifier<T>(T tileModifier) => TileModifier = (int)(object)tileModifier!;
+
+  /// <summary>
+  /// Returns the tile building
+  /// </summary>
+  /// <typeparam name="T">The tile building enum</typeparam>
+  /// <returns>the tile building as <c>T</c></returns>
+  public T GetTileBuilding<T>() => (T)(object)TileBuilding;
+
+  /// <summary>
+  /// Sets the tile building
+  /// </summary>
+  /// <param name="tileBuilding">the tile building enum value</param>
+  public void SetTileBuilding(object tileBuilding) => TileBuilding = (int)tileBuilding;
+
+  /// <summary>
+  /// Sets the tile building
+  /// </summary>
+  /// <param name="tileBuilding">the tile building enum value</param>
+  /// <typeparam name="T">The tile building enum</typeparam>
+  public void SetTileBuilding<T>(T tileBuilding) => TileBuilding = (int)(object)tileBuilding!;
 }
 
 /// <summary>
@@ -147,7 +182,7 @@ public enum VillageTileModifier {
   City = 1
 }
 
-public enum FieldTileBuildings {
+public enum FieldTileBuilding {
   None = 0,
   Sawmill = 1,
   Windmill = 2,
@@ -157,6 +192,68 @@ public enum FieldTileBuildings {
   Altar = 6
 }
 
+public enum ForestTileBuilding {
+  None = 0,
+  LumberHut = 1,
+  Temple = 2
+}
+
+public enum MountainTileBuilding {
+  None = 0,
+  Mine = 1,
+  Temple = 2
+}
+
+public enum WaterTileBuilding {
+  None = 0,
+  Port = 1,
+  Temple = 2
+}
+
+public enum OceanTileBuilding {
+  None = 0,
+  Temple = 1
+}
+
+public enum VillageTileBuilding {
+  None = 0
+}
+
 public static class BooleanExtensions {
+  /// <summary>
+  /// Converts the bool to a <see cref="ulong"/>
+  /// </summary>
+  /// <param name="value">the bool to convert</param>
+  /// <returns>1 if value else 0</returns>
   public static ulong ToULong(this bool value) => value ? 1ul : 0;
+}
+
+public static class ULongExtensions {
+  /// <summary>
+  /// Clear the bits in the specified position by the specified length.
+  /// Ex. value == 3 (...011); value.ClearBits(1, 1); value == 1 (...001)
+  /// </summary>
+  /// <param name="value">the number to clear bits</param>
+  /// <param name="bits">number of bits to clear; must be all ones</param>
+  /// <param name="position">the position where to start clearing bits starting from the right</param>
+  public static ulong ClearBits(this ulong value, int bits, int position) => value & ~((ulong)bits << position);
+
+  /// <summary>
+  /// Clear bits and set them
+  /// </summary>
+  /// <param name="value">the number where to set bits</param>
+  /// <param name="data">the bits to be set</param>
+  /// <param name="bits">number of bits to set; must be all ones</param>
+  /// <param name="position">the position where to set bits starting from the right</param>
+  public static void SetBits(this ref ulong value, ulong data, int bits, int position) =>
+    value = value.ClearBits(bits, position) | (data << position);
+
+  /// <summary>
+  /// Get the bits
+  /// </summary>
+  /// <param name="value">the number where to get bits</param>
+  /// <param name="bits">number of bits to get; must be all ones</param>
+  /// <param name="position">the position where to get bits starting from the right</param>
+  /// <returns></returns>
+  public static ulong GetBits(this ulong value, int bits, int position) => (value >> position) & (ulong)bits;
 }
