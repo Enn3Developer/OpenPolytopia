5cb3099604a1aafdef3a7fd9447ffeb9864bf467
Enn3Developer <arturpeshko39@gmail.com>
2025-03-04 19:04:39 +0100

    completed lobby management

--- OpenPolytopia.Server/GameServer.cs
+++ OpenPolytopia.Server/GameServer.cs
@@ -19,13 +19,24 @@ public class GameServer : IDisposable {
 
   private async Task<bool> ManagePacketAsync(uint id, IPacket packet, NetworkStream stream, List<byte> bytes) {
     switch (packet) {
+      // Handshake packet
+      // Respond with the result of the check
       case HandshakePacket handshakePacket:
         await stream.WritePacketAsync(new HandshakeResponsePacket { Ok = handshakePacket.Version == "0.1.0" }, bytes);
         break;
+      // Register the username
       case RegisterUserPacket registerUserPacket:
+        foreach (var keyValue in _playerNames) {
+          if (keyValue.Value == registerUserPacket.Name) {
+            _playerNames.TryRemove(keyValue.Key, out _);
+          }
+        }
+
         _playerNames[id] = registerUserPacket.Name;
         await stream.WritePacketAsync(new RegisterUserResponsePacket { Ok = true }, bytes);
         break;
+      // Add a player to a lobby
+      // Respond with the result of the operation
       case LobbyConnectPacket lobbyConnectPacket:
         var ok = _lobbyManager.AddPlayer(lobbyConnectPacket.Id, _playerNames[id]);
         if (ok) {
@@ -34,16 +45,27 @@ public class GameServer : IDisposable {
 
         await stream.WritePacketAsync(new LobbyConnectResponsePacket { Ok = ok }, bytes);
         break;
+      // Create a new lobby
       case CreateLobbyPacket createLobbyPacket:
         var lobby = _lobbyManager.NewLobby(createLobbyPacket.MaxPlayers);
         Broadcast(new LobbyUpdatePacket { Lobby = lobby });
         await stream.WritePacketAsync(new CreateLobbyResponsePacket { Ok = true }, bytes);
         break;
+      // Respond with the lobbies currently available
       case GetLobbiesPacket:
         await stream.WritePacketAsync(new GetLobbiesResponsePacket { Lobbies = _lobbyManager.Lobbies }, bytes);
         break;
+      // Disconnect from a lobby
       case LobbyDisconnectPacket lobbyDisconnectPacket:
         _lobbyManager.RemovePlayer(lobbyDisconnectPacket.Id, _playerNames[id]);
+        if (_lobbyManager[lobbyDisconnectPacket.Id] != null) {
+          var lobbyToDelete = _lobbyManager[lobbyDisconnectPacket.Id]!;
+          if (lobbyToDelete.Players.Count == 0) {
+            _lobbyManager.Lobbies.Remove(lobbyToDelete);
+            Broadcast(new LobbyDeletedPacket { Id = lobbyDisconnectPacket.Id });
+          }
+        }
+
         await stream.WritePacketAsync(new LobbyDisconnectResponsePacket { Ok = true }, bytes);
         break;
     }
