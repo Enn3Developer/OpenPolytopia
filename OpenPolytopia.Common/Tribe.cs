34c7c81a645815a199d980d7e06659886b991f06
Enn3Developer <arturpeshko39@gmail.com>
2025-03-08 21:32:20 +0100

    added docs

--- OpenPolytopia.Common/Tribe.cs
+++ OpenPolytopia.Common/Tribe.cs
@@ -6,17 +6,59 @@ public class Tribe {
   public required TerrainRate TerrainRate { get; init; }
 }
 
+/// <summary>
+/// Spawn rates for every tribe
+/// </summary>
+/// <remarks>
+/// For every field there's the initial value, these rates are multipliers
+/// </remarks>
 public class SpawnRate {
+  /// <summary>
+  /// Initial value = 0.18
+  /// </summary>
   public required float FruitRate { get; init; }
+
+  /// <summary>
+  /// Initial value = 0.18
+  /// </summary>
   public required float CropRate { get; init; }
+
+  /// <summary>
+  /// Initial value = 0.19
+  /// </summary>
   public required float AnimalRate { get; init; }
+
+  /// <summary>
+  /// Initial value = 0.5
+  /// </summary>
   public required float FishRate { get; init; }
+
+  /// <summary>
+  /// Initial value = 0.11
+  /// </summary>
   public required float MineralRate { get; init; }
 }
 
+/// <summary>
+/// Terrain rates for every tribe
+/// </summary>
+/// <remarks>
+/// For every field there's the initial value, these rates are multipliers
+/// </remarks>
 public class TerrainRate {
+  /// <summary>
+  /// Initial value = 0.38
+  /// </summary>
   public required float ForestRate { get; init; }
+
+  /// <summary>
+  /// Initial value = 0.14
+  /// </summary>
   public required float MountainRate { get; init; }
+
+  /// <summary>
+  /// No initial value because it depends on the map type
+  /// </summary>
   public required float WaterRate { get; init; }
 }
 
