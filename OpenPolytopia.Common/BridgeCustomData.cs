8efd3b9605a56a5a7902bb64bad5f1d3d3a64461
GamerPlayer888 <66787796+GamerPlayer888@users.noreply.github.com>
2025-03-12 14:02:27 +0100

    Changes in BridgeCustomData
    Assign value to Direction

--- OpenPolytopia.Common/BridgeCustomData.cs
+++ OpenPolytopia.Common/BridgeCustomData.cs
@@ -26,7 +26,7 @@ public struct BridgeData : ITileCustomData {
   /// </summary>
   /// <param name="value">value to deserialize from</param>
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  public void FromULong(ulong value) => _ = (BridgeDirection)value.GetBits(ONE_BIT, 0);
+  public void FromULong(ulong value) => Direction = (BridgeDirection)value.GetBits(ONE_BIT, 0);
 
   /// <summary>
   /// Serializes the data to a <see langword="ulong"/>
